cdb95658f4eb4c54c2d5ca512cd10c75c7d85967
Ken Yoshioka <ken_yoshioka@dwango.co.jp>
2018-11-19 12:42:06 +0900

    added exception at a message event

--- script/self/message_event_manager.js
+++ script/self/message_event_manager.js
@@ -10,15 +10,6 @@
 // Initialization
 // var player                     = require('./player');
 var scene;
-var events = {
-	// pointer_other_local_down: pointer.other_local_down,
-	// pointer_other_local_move: pointer.other_local_move,
-	// pointer_other_local_up: pointer.other_local_up,
-	eval_function: eval_function,
-	// get_piece: local_get_piece,
-	// move_piece: local_move_piece,
-	// place_piece: local_place_piece,
-};
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function eval_function() {}
@@ -26,6 +17,15 @@ function pass_through() {}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function set_scene(sc) {
 	scene = sc;
+	var events = {
+		// pointer_other_local_down: pointer.other_local_down,
+		// pointer_other_local_move: pointer.other_local_move,
+		// pointer_other_local_up: pointer.other_local_up,
+		eval_function: eval_function,
+		// get_piece: local_get_piece,
+		// move_piece: local_move_piece,
+		// place_piece: local_place_piece,
+	};
 	scene.message.add(function(mes) {
 		try {
 			if (mes === undefined) return;
